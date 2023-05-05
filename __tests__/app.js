4d256fe0953a8d77269757972545100165ae2050
Mathieu Soul <mathieus@sicara.com>
2023-05-05 17:03:11 +0200

    Refactor tests

--- __tests__/app.js
+++ __tests__/app.js
@@ -3,87 +3,79 @@ const path = require("path");
 const assert = require("yeoman-assert");
 const helpers = require("yeoman-test");
 
-describe("generator-sicarator:app:defaults", () => {
-  beforeAll(() => {
-    return helpers
-      .run(path.join(__dirname, "../generators/app"))
-      .withPrompts({
-        projectName: "project-name",
-        projectDescription: "Project Description",
-        pythonVersion: "3.9.13" // Python version depends on the docker image of CI
-      })
-      .withLocalConfig({});
-  });
+const COMMON_FILES_PATHS = [
+  "README.md",
+  ".gitignore",
+  ".yo-rc.json",
+  "poetry.lock",
+  "pyproject.toml"
+];
+const HELLO_WORLD_FILES_PATHS = [
+  "src/hello_world.py",
+  "tests/test_hello_world.py"
+];
+const API_FILES_PATHS = [
+  "src/api/main.py",
+  "src/api/types.py",
+  "tests/api/test_main.py"
+];
 
-  it("creates README.md", () => {
-    assert.file(["README.md"]);
-  });
+describe("generator-sicarator:app", () => {
+  describe("Sicarator with default answers", () => {
+    beforeAll(() => {
+      return helpers
+        .run(path.join(__dirname, "../generators/app"))
+        .withPrompts({
+          projectName: "project-name",
+          projectDescription: "Project Description",
+          pythonVersion: "3.9.13" // Python version depends on the docker image of CI
+        })
+        .withLocalConfig({});
+    });
 
-  it("creates .gitignore", () => {
-    assert.file([".gitignore"]);
-  });
+    it("creates common files", () => {
+      assert.file(COMMON_FILES_PATHS);
+    });
 
-  it("creates .yo-rc.json", () => {
-    assert.file([".yo-rc.json"]);
-  });
+    it("creates hello-world files", () => {
+      assert.file(HELLO_WORLD_FILES_PATHS);
+    });
 
-  it("creates hello-world files", () => {
-    assert.file(["src/hello_world.py", "tests/test_hello_world.py"]);
-  });
+    it("does not create API files", () => {
+      assert.noFile(API_FILES_PATHS);
+    });
 
-  it("does not create API files", () => {
-    assert.noFile([
-      "src/api/main.py",
-      "src/api/types.py",
-      "tests/api/test_main.py"
-    ]);
+    it("has correct Python version", () => {
+      assert.fileContent("pyproject.toml", 'python = "3.9.13"');
+    });
   });
+  describe("Sicarator with includeApi set to true", () => {
+    beforeAll(() => {
+      return helpers
+        .run(path.join(__dirname, "../generators/app"))
+        .withPrompts({
+          projectName: "project-name",
+          projectDescription: "Project Description",
+          pythonVersion: "3.9.13", // Python version depends on the docker image of CI
+          includeApi: true
+        })
+        .withLocalConfig({});
+    });
 
-  it("creates correct poetry files", () => {
-    assert.file(["poetry.lock", "pyproject.toml"]);
-    assert.fileContent("pyproject.toml", 'python = "3.9.13"');
-  });
-});
+    it("creates common files", () => {
+      assert.file(COMMON_FILES_PATHS);
+    });
 
-describe("generator-sicarator:app:includeApi", () => {
-  beforeAll(() => {
-    return helpers
-      .run(path.join(__dirname, "../generators/app"))
-      .withPrompts({
-        projectName: "project-name",
-        projectDescription: "Project Description",
-        pythonVersion: "3.9.13", // Python version depends on the docker image of CI
-        includeApi: true
-      })
-      .withLocalConfig({});
-  });
+    it("does not create hello-world files", () => {
+      assert.noFile(HELLO_WORLD_FILES_PATHS);
+    });
 
-  it("creates README.md", () => {
-    assert.file(["README.md"]);
-  });
+    it("creates API files", () => {
+      assert.file(API_FILES_PATHS);
+    });
 
-  it("creates .gitignore", () => {
-    assert.file([".gitignore"]);
-  });
-
-  it("creates .yo-rc.json", () => {
-    assert.file([".yo-rc.json"]);
-  });
-
-  it("does not create hello-world files", () => {
-    assert.noFile(["src/hello_world.py", "tests/test_hello_world.py"]);
-  });
-
-  it("creates API files", () => {
-    assert.file([
-      "src/api/main.py",
-      "src/api/types.py",
-      "tests/api/test_main.py"
-    ]);
-  });
-
-  it("creates correct poetry files", () => {
-    assert.file(["poetry.lock", "pyproject.toml"]);
-    assert.fileContent("pyproject.toml", 'python = "3.9.13"');
+    it("has correct Python version", () => {
+      assert.fileContent("pyproject.toml", 'python = "3.9.13"');
+    });
   });
 });
