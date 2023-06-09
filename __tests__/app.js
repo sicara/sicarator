bf7b24de0fdd807627c3c97ac016aab921ea83a3
Yannick Wolff <ybwolff@gmail.com>
2023-06-09 19:36:10 +0200

    Refactor tests with describe.each

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,11 +1,9 @@
 "use strict";
-const path = require("path");
 const assert = require("yeoman-assert");
 const helpers = require("yeoman-test");
+const path = require("path");
 
 jest.setTimeout(60000); // 1 minute timeout for poetry lock command
-
-const PYTHON_VERSION = "3.11.4"; // Should match the version used in the CI (see .circleci/config.yml)
 
 const COMMON_FILES_PATHS = [
   "src",
@@ -27,219 +25,241 @@ const API_FILES_PATHS = [
 ];
 const TERRAFORM_FILES_PATHS = ["terraform/main.tf", "terraform/variables.tf"]; // Not exhaustive
 const DVC_FILES_PATHS = [".dvcignore", ".dvc/config", ".dvc/.gitignore"];
+const STREAMLIT_FILES_PATHS = ["src/streamlit_app"];
+
+const PROJECT_NAME = "Sicarator Test";
+const PROJECT_SLUG = "sicarator-test";
+const LAST_PYTHON_VERSION = "3.11.4";
+const TERRAFORM_BACKEND_BUCKET_NAME = "terraform-backend-bucket-name";
+const AWS_ACCOUNT_ID = "1234567890";
+const AWS_REGION = "us-east-1";
+const GCP_PROJECT_ID = "gcp-project-id";
+const GCP_REGION = "us-west1";
+
+const DEFAULT_ANSWERS = {
+  pythonVersion: LAST_PYTHON_VERSION,
+  includeApi: false,
+  apiInfrastructure: null,
+  includeDvc: false,
+  includeStreamlit: false,
+  includeHelloWorld: false,
+  awsRegion: null,
+  awsAccountId: null,
+  terraformBackendBucketName: null
+};
+const ALL_OPTIONS_EXCEPT_INFRA_ANSWERS = {
+  includeApi: true,
+  includeDvc: true,
+  includeStreamlit: true
+};
+const AWS_INFRA_ANSWERS = {
+  terraformBackendBucketName: TERRAFORM_BACKEND_BUCKET_NAME,
+  awsAccountId: AWS_ACCOUNT_ID,
+  awsRegion: AWS_REGION
+};
+const GCP_INFRA_ANSWERS = {
+  terraformBackendBucketName: TERRAFORM_BACKEND_BUCKET_NAME,
+  gcpProjectId: GCP_PROJECT_ID,
+  gcpRegion: GCP_REGION
+};
 
 describe("generator-sicarator:app", () => {
-  describe("Sicarator with default answers", () => {
-    beforeAll(() => {
-      return helpers
-        .run(path.join(__dirname, "../generators/app"))
-        .withPrompts({
-          projectName: "project-name",
-          pythonVersion: PYTHON_VERSION,
-          includeApi: false,
-          includeHelloWorld: true,
-          includeDvc: false
-        })
-        .withLocalConfig({});
-    });
+  describe.each([
+    {
+      description: "no option",
+      ...DEFAULT_ANSWERS
+    },
+    {
+      description: "API",
+      ...DEFAULT_ANSWERS,
+      includeApi: true
+    },
+    {
+      description: "API & AWS infra",
+      ...DEFAULT_ANSWERS,
+      includeApi: true,
+      ...AWS_INFRA_ANSWERS
+    },
+    {
+      description: "API & GCP infra",
+      ...DEFAULT_ANSWERS,
+      includeApi: true,
+      ...GCP_INFRA_ANSWERS
+    },
+    {
+      description: "DVC",
+      ...DEFAULT_ANSWERS,
+      includeDvc: true
+    },
+    {
+      description: "Streamlit",
+      ...DEFAULT_ANSWERS,
+      includeStreamlit: true
+    },
+    {
+      description: "HelloWorld",
+      ...DEFAULT_ANSWERS,
+      includeHelloWorld: true
+    },
+    {
+      description: "all options & AWS infra",
+      ...DEFAULT_ANSWERS,
+      ...ALL_OPTIONS_EXCEPT_INFRA_ANSWERS,
+      ...AWS_INFRA_ANSWERS
+    },
+    {
+      description: "all options & GCP infra",
+      ...DEFAULT_ANSWERS,
+      ...ALL_OPTIONS_EXCEPT_INFRA_ANSWERS,
+      ...GCP_INFRA_ANSWERS
+    },
+    {
+      description: "Python 3.10 (all options & AWS infra)",
+      ...DEFAULT_ANSWERS,
+      ...ALL_OPTIONS_EXCEPT_INFRA_ANSWERS,
+      ...AWS_INFRA_ANSWERS,
+      pythonVersion: "3.10.11"
+    },
+    {
+      description: "Python 3.9 (all options & AWS infra)",
+      ...DEFAULT_ANSWERS,
+      ...ALL_OPTIONS_EXCEPT_INFRA_ANSWERS,
+      ...AWS_INFRA_ANSWERS,
+      pythonVersion: "3.9.16"
+    },
+    {
+      description: "Python 3.8 (all options & AWS infra)",
+      ...DEFAULT_ANSWERS,
+      ...ALL_OPTIONS_EXCEPT_INFRA_ANSWERS,
+      ...AWS_INFRA_ANSWERS,
+      pythonVersion: "3.8.16"
+    }
+  ])(
+    "Generate project with $description",
+    ({
+      pythonVersion,
+      includeApi,
+      apiInfrastructure,
+      includeDvc,
+      includeStreamlit,
+      includeHelloWorld,
+      awsRegion,
+      awsAccountId,
+      terraformBackendBucketName
+    }) => {
+      beforeAll(() => {
+        return helpers
+          .run(path.join(__dirname, "../generators/app"))
+          .withPrompts({
+            projectName: PROJECT_NAME,
+            pythonVersion,
+            includeApi,
+            apiInfrastructure,
+            includeDvc,
+            includeStreamlit,
+            includeHelloWorld,
+            awsRegion,
+            awsAccountId,
+            terraformBackendBucketName
+          })
+          .withLocalConfig({});
+      });
 
-    it("creates common files", () => {
-      assert.file(COMMON_FILES_PATHS);
-    });
+      it("creates common files", () => {
+        assert.file(COMMON_FILES_PATHS);
+      });
 
-    it("creates poetry.lock", () => {
-      assert.file("poetry.lock");
-    });
+      it("creates poetry.lock", () => {
+        assert.file("poetry.lock");
+      });
 
-    it("creates .gitignore", () => {
-      assert.file(".gitignore");
-    });
+      it("creates .gitignore", () => {
+        assert.file(".gitignore");
+      });
 
-    it("creates .yo-rc.json", () => {
-      assert.file(".yo-rc.json");
-    });
+      it("creates .git folder", () => {
+        assert.file(".git");
+      });
 
-    it("creates hello-world files", () => {
-      assert.file(HELLO_WORLD_FILES_PATHS);
-    });
+      it("creates .yo-rc.json", () => {
+        assert.file(".yo-rc.json");
+      });
 
-    it("does not create API files", () => {
-      assert.noFile(API_FILES_PATHS);
-    });
+      it("creates hello-world files when needed", () => {
+        if (includeHelloWorld) {
+          assert.file(HELLO_WORLD_FILES_PATHS);
+        } else {
+          assert.noFile(HELLO_WORLD_FILES_PATHS);
+        }
+      });
 
-    it("does not create DVC files", () => {
-      assert.noFile(DVC_FILES_PATHS);
-    });
+      it("creates API files when needed", () => {
+        if (includeApi) {
+          assert.file(API_FILES_PATHS);
+        } else {
+          assert.noFile(API_FILES_PATHS);
+        }
+      });
 
-    it("creates .git folder", () => {
-      assert.file(".git");
-    });
+      it("creates API infrastructure code when needed", () => {
+        if (apiInfrastructure === null) {
+          assert.noFile(TERRAFORM_FILES_PATHS);
+        } else {
+          assert.file(TERRAFORM_FILES_PATHS);
+        }
+      });
 
-    it("has correct Python version", () => {
-      assert.fileContent("pyproject.toml", `python = "${PYTHON_VERSION}"`);
-    });
-  });
-  describe("Sicarator with API, without Terraform", () => {
-    beforeAll(() => {
-      return helpers
-        .run(path.join(__dirname, "../generators/app"))
-        .withPrompts({
-          projectName: "project-name",
-          pythonVersion: PYTHON_VERSION,
-          includeApi: true,
-          apiInfrastructure: null,
-          includeDvc: false
-        })
-        .withLocalConfig({});
-    });
+      it("creates DVC files when needed", () => {
+        if (includeDvc) {
+          assert.file(DVC_FILES_PATHS);
+        } else {
+          assert.noFile(DVC_FILES_PATHS);
+        }
+      });
 
-    it("creates common files", () => {
-      assert.file(COMMON_FILES_PATHS);
-    });
+      it("creates Streamlit files when needed", () => {
+        if (includeStreamlit) {
+          assert.file(STREAMLIT_FILES_PATHS);
+        } else {
+          assert.noFile(STREAMLIT_FILES_PATHS);
+        }
+      });
 
-    it("creates poetry.lock", () => {
-      assert.file("poetry.lock");
-    });
+      it("has correct project slug", () => {
+        assert.fileContent("pyproject.toml", `name = "${PROJECT_SLUG}"`);
+      });
 
-    it("does not create hello-world files", () => {
-      assert.noFile(HELLO_WORLD_FILES_PATHS);
-    });
+      it("has correct Python version", () => {
+        assert.fileContent("pyproject.toml", `python = "${pythonVersion}"`);
+      });
 
-    it("creates API files", () => {
-      assert.file(API_FILES_PATHS);
-    });
+      it("has correct terraform backend bucket name", () => {
+        if (apiInfrastructure !== null) {
+          assert.fileContent(
+            "terraform/backend.tf",
+            `bucket  = "${terraformBackendBucketName}"`
+          );
+        }
+      });
 
-    it("does not create terraform files", () => {
-      assert.noFile(TERRAFORM_FILES_PATHS);
-    });
-  });
-  describe("Sicarator with API and Terraformed AWS infrastructure", () => {
-    beforeAll(() => {
-      return helpers
-        .run(path.join(__dirname, "../generators/app"))
-        .withPrompts({
-          projectName: "project-name",
-          pythonVersion: PYTHON_VERSION,
-          includeApi: true,
-          apiInfrastructure: "aws",
-          terraformBackendBucketName: "terraform-backend-bucket-name",
-          awsRegion: "eu-west-1",
-          awsAccountId: "1234567890",
-          includeNatGateway: true,
-          includeDvc: false
-        })
-        .withLocalConfig({});
-    });
+      it("has correct AWS account URL", () => {
+        if (apiInfrastructure === "aws") {
+          assert.fileContent(
+            "Makefile",
+            `AWS_ACCOUNT_URL=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com`
+          );
+        }
+      });
 
-    it("creates common files", () => {
-      assert.file(COMMON_FILES_PATHS);
-    });
-
-    it("creates poetry.lock", () => {
-      assert.file("poetry.lock");
-    });
-
-    it("does not create hello-world files", () => {
-      assert.noFile(HELLO_WORLD_FILES_PATHS);
-    });
-
-    it("creates API files", () => {
-      assert.file(API_FILES_PATHS);
-    });
-
-    it("creates terraform files", () => {
-      assert.file(TERRAFORM_FILES_PATHS);
-    });
-
-    it("has correct terraform backend bucket name", () => {
-      assert.fileContent(
-        "terraform/backend.tf",
-        'bucket  = "terraform-backend-bucket-name"'
-      );
-    });
-
-    it("has correct AWS account URL", () => {
-      assert.fileContent(
-        "Makefile",
-        "AWS_ACCOUNT_URL=1234567890.dkr.ecr.eu-west-1.amazonaws.com"
-      );
-    });
-  });
-  describe("Sicarator with API and Terraformed GCP infrastructure", () => {
-    beforeAll(() => {
-      return helpers
-        .run(path.join(__dirname, "../generators/app"))
-        .withPrompts({
-          projectName: "project-name",
-          pythonVersion: PYTHON_VERSION,
-          includeApi: true,
-          apiInfrastructure: "gcp",
-          terraformBackendBucketName: "terraform-backend-bucket-name",
-          gcpRegion: "europe-west9-a",
-          gcpProjectId: "my-gcp-project-id",
-          includeNatGateway: true,
-          includeDvc: false
-        })
-        .withLocalConfig({});
-    });
-
-    it("creates common files", () => {
-      assert.file(COMMON_FILES_PATHS);
-    });
-
-    it("creates poetry.lock", () => {
-      assert.file("poetry.lock");
-    });
-
-    it("does not create hello-world files", () => {
-      assert.noFile(HELLO_WORLD_FILES_PATHS);
-    });
-
-    it("creates API files", () => {
-      assert.file(API_FILES_PATHS);
-    });
-
-    it("creates terraform files", () => {
-      assert.file(TERRAFORM_FILES_PATHS);
-    });
-
-    it("has correct terraform backend bucket name", () => {
-      assert.fileContent(
-        "terraform/backend.tf",
-        'bucket = "terraform-backend-bucket-name"'
-      );
-    });
-
-    it("has correct GCP project ID", () => {
-      assert.fileContent("Makefile", "GCP_PROJECT_ID=my-gcp-project-id");
-      assert.fileContent(
-        "terraform/variables.tf",
-        'default     = "my-gcp-project-id"'
-      );
-    });
-  });
-  describe("Sicarator with DVC", () => {
-    beforeAll(() => {
-      return helpers
-        .run(path.join(__dirname, "../generators/app"))
-        .withPrompts({
-          projectName: "project-name",
-          pythonVersion: PYTHON_VERSION,
-          includeDvc: true
-        })
-        .withLocalConfig({});
-    });
-
-    it("creates common files", () => {
-      assert.file(COMMON_FILES_PATHS);
-    });
-
-    it("creates poetry.lock", () => {
-      assert.file("poetry.lock");
-    });
-
-    it("creates DVC files", () => {
-      assert.file(DVC_FILES_PATHS);
-    });
-  });
+      it("has correct GCP project ID", () => {
+        if (apiInfrastructure === "gcp") {
+          assert.fileContent("Makefile", `GCP_PROJECT_ID=${GCP_PROJECT_ID}`);
+          assert.fileContent(
+            "terraform/variables.tf",
+            `default     = "${GCP_PROJECT_ID}"`
+          );
+        }
+      });
+    }
+  );
 });
